7069ed980cdcc2690f0db1f1c965b5f21b5c04a4
adams2a7 <adams2a7@mail.uc.edu>
2021-02-08 17:48:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*
+Lab 4
+Partners: Alyssa Adams and Jillian Sizemore
+Date: 9 February 2021
+*/
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
