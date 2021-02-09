ac646d51642f9ac36620b42e0b948d70afced893
adams2a7 <adams2a7@mail.uc.edu>
2021-02-09 15:36:42 +0000

    met with Jillian and completed lab. added loop and if statements

--- Main.java
+++ Main.java
@@ -3,8 +3,29 @@ Lab 4
 Partners: Alyssa Adams and Jillian Sizemore
 Date: 9 February 2021
 */
+import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  Random r = new Random();
+
+  int randomInt = r.nextInt(50)+1;
+  
+  for(int count = randomInt; count >= 0; count--)
+   {	
+    System.out.println(count);
+    }
+
+  System.out.println("The Random Number is: " +randomInt);
+
+ 
+  if (randomInt <= 5 ){
+    System.out.println("Ahoy Mateys!"); 
+  }
+  if (randomInt > 25 && randomInt< 42){
+    System.out.println("Cannonball!"); 
+  }
+  else {System.out.println("Blast Off!"); }
+  
   }
 }
